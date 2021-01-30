8a038b43a4c1d042638c751c471af4416180466f
JuanAntonio609 <73108839+JuanAntonio609@users.noreply.github.com>
2021-01-30 08:30:04 +0100

    Main Menu v.2
    Mas menu y mas escenas, scripts añadidos y cosas

---
+++ Assets/Scripts/Controllers/SceneController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneController : MonoBehaviour
+{
+    
+
+    #region  ##############  METHODS  ##############
+    public string sceneName;
+
+    /// <summary>
+    /// Cambia la escena recibida como parametro
+    /// </summary>
+    /// <param name="sceneName"></param>
+    public void ChangeScene(string sceneName) {
+        // restauramos la escala de tiempo default
+        Time.timeScale = 1f;
+        // cargamos la escena
+        SceneManager.LoadScene(sceneName);
+    }
+
+    /// <summary>
+    /// Cerrar el ejecutable del juego
+    /// </summary>
+    public void ExitGame() {
+        // cierra la aplicación
+        Application.Quit();
+    }
+    #endregion
+}
---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour
+{
+    #region ############### VARIABLES ###############
+    public AudioMixer audioMixer;
+    #endregion
+    #region ############### EVENTS ###############
+
+    private void Awake() {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    #endregion
+
+
+    #region ################ METHODS ################
+    /// <summary>
+    /// configura el volumen del canal master
+    /// </summary>
+    /// <param name="volume"></param>
+    public void SetMasterVolume(float volume) {
+        audioMixer.SetFloat("MasterVolume", volume);
+    }
+
+
+    /// <summary>
+    /// configura el volumen del canal de sonido
+    /// </summary>
+    /// <param name="volume"></param>
+    public void SetSoundVolume(float volume) {
+        audioMixer.SetFloat("EFXVolume", volume);
+    }
+
+    /// <summary>
+    /// configura el volumen del canal de musica
+    /// </summary>
+    /// <param name="volume"></param>
+    public void SetMusicVolume(float volume) {
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+
+    #endregion
+}
