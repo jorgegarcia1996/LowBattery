c8bc48a14fc53e90dc4cdf49fa8cb6aadbe13a75
JuanAntonio609 <73108839+JuanAntonio609@users.noreply.github.com>
2021-01-30 12:15:20 +0100

    Main menu & game menu
    po eso

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     #region ##### VARIABLES #####
+    public GameObject inGameHud;
+    public GameObject gameOverMenu;
+    public GameObject pauseMenu;
+    public CameraController mainCamera;
+    public PlayerControler player;
+
+    public bool paused;
 
     public static GameManager instance;
     #endregion
-
     #region ##### EVENTS #####
     void Start() {
         if (instance == null) {
@@ -16,8 +22,55 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    #endregion
+    void Update() {
+        if (Input.GetButtonDown("Cancel")) {
+            Pause(!paused);
+        }
+    }
 
+    #endregion
     #region ##### METHODS #####
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void Pause(bool pause) {
+        if (pause) {
+            pauseMenu.SetActive(true);
+            inGameHud.SetActive(false);
+            mainCamera.enabled = false;
+            player.enabled = false;
+            Time.timeScale = 0f;
+            paused = true;
+        }
+        else {
+            pauseMenu.SetActive(false);
+            inGameHud.SetActive(true);
+            mainCamera.enabled = true;
+            player.enabled = true;
+            Time.timeScale = 1f;
+            paused = false;
+        }
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    public void RestartLevel() {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    /// <summary>
+    /// Game Over Menu
+    /// </summary>
+    public void GameOver() {
+        inGameHud.SetActive(false);
+        gameOverMenu.SetActive(true);
+        gameOverMenu.transform.Find("GameOverMenu").gameObject.SetActive(true);
+    }
+
+    public void SetMouseSensibility(float sensitivity) {
+        mainCamera.mouseSensitivity = sensitivity;
+        
+    }
     #endregion
 }
